96e54dbc88d11dccadb7562b179a596ef02b3187
cshaoy <108863789+cshaoy@users.noreply.github.com>
2022-08-07 17:49:13 +0800

    Add files via upload

---
+++ csy/贪吃蛇/js/tanchishe.js
@@ -0,0 +1,201 @@
+var snakebody = [], food;
+let direction = 'x';
+let score = 0;
+let time;
+let off = document.getElementById('off');
+let scoreinput = document.getElementById('score');
+let container = document.getElementById("container");
+let isturn = false;
+
+off.onclick = function () {
+    let text = off.innerText;
+    if (text == '继续游戏' || text == '开始游戏') {
+        time = setInterval(function () {
+            move()
+            off.blur();
+        }, 500);
+        off.innerText = '暂停游戏';
+    } else if (text == '暂停游戏') {
+        clearInterval(time);
+        off.innerText = '继续游戏';
+    } else if (text == '重新开始') {
+        score = 0;
+        snakebody = [];
+        food = null;
+        direction = 'x'
+        restart();
+        time = setInterval(function () {
+            move()
+            off.blur();
+        }, 500);
+        off.innerText = '暂停游戏';
+    }
+
+};
+
+function establishsnakebody() {
+
+    snake(6);
+    for (let index = 0; index < snakebody.length; index++) {
+        const element = snakebody[index];
+        element.style.top = '0px';
+        element.style.left = 0 + index * 25 + 'px';
+
+        container.appendChild(element)
+
+
+    }
+    establifood()
+}
+establishsnakebody();
+function snake(num) {
+
+    for (let index = 0; index < num; index++) {
+        let size = document.createElement('div');
+        size.classList.add('snakesize')
+        snakebody.push(size);
+    }
+
+
+}
+
+
+function move() {
+    let size1 = snakebody[0];
+    let size2 = snakebody[snakebody.length - 1];
+    let size2x = size2.offsetLeft;
+    let size2y = size2.offsetTop;
+    let x, y
+    if (direction == 'x') {
+        if (size2x + 25 >= 800) {
+            clearInterval(time);
+            off.innerText = '重新开始';
+            return;
+        }
+        x = size2x + 25;
+        y = size2y;
+
+    }
+    if (direction == '-x') {
+        if (size2x - 25 < 0) {
+            clearInterval(time);
+            off.innerText = '重新开始';
+            return;
+        }
+        x = size2x - 25;
+        y = size2y;
+    }
+    if (direction == 'y') {
+        if (size2y + 25 >= 800) {
+            clearInterval(time);
+            off.innerText = '重新开始';
+            return;
+        }
+        x = size2x;
+        y = size2y + 25;
+    }
+    if (direction == '-y') {
+        if (size2y - 25 < 0) {
+            clearInterval(time);
+            off.innerText = '重新开始';
+            return;
+        }
+        x = size2x;
+        y = size2y - 25;
+    }
+    let iscollision = collision(x, y);
+    if (iscollision == true) {
+        clearInterval(time);
+        off.innerText = '重新开始';
+        return;
+    }
+    snakebody.shift();
+    if (foodposition[0] == x && foodposition[1] == y) {
+        snakebody.splice(0, 0, food);
+        establifood();
+        score += 10;
+        scoreinput.innerText = score;
+    }
+    size1.style.left = x + 'px';
+    size1.style.top = y + 'px';
+
+    snakebody.push(size1);
+    isturn = true;
+}
+
+
+
+
+document.addEventListener('keydown', onkeydown, false);
+
+function onkeydown(event) {
+    if (isturn == false) return;
+
+    switch (event.key) {
+        case 'w':
+            if (direction != 'y') {
+                direction = '-y';
+                isturn = false
+            }
+            break;
+        case 'a':
+            if (direction != 'x') {
+                direction = '-x';
+                isturn = false
+            }
+            break;
+        case 's':
+            if (direction != '-y') {
+                direction = 'y';
+                isturn = false
+            }
+            break;
+        case 'd':
+            if (direction != '-x') {
+                direction = 'x';
+                isturn = false
+            }
+            break;
+    }
+}
+
+function establifood() {
+    let x, y;
+
+    while (true) {
+        x = Math.ceil(Math.random() * 10 * 3.2) * 25;
+        y = Math.ceil(Math.random() * 10 * 3.2) * 25;
+        let iscollision = collision(x, y);
+        if (!iscollision) {
+            break;
+        }
+    }
+
+    let size = document.createElement('div');
+    size.classList.add('snakesize');
+    size.style.top = y + 'px';
+    size.style.left = x + 'px';
+    container.appendChild(size)
+    foodposition = [x, y];
+    food = size;
+}
+function collision(x, y) {
+    let iscollision = false;
+    for (let index = 1; index < snakebody.length; index++) {
+        const element = snakebody[index];
+        let sizex = element.offsetLeft;
+        let sizey = element.offsetTop;
+
+        console.log(x == sizex && y == sizey)
+        if (x == sizex && y == sizey) {
+            iscollision = true;
+            return iscollision;
+        }
+    }
+    return iscollision;
+}
+
+function restart() {
+    container.innerHTML = '';
+    establishsnakebody();
+}
